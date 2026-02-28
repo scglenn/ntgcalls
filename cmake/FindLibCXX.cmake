78a8af5e2e9ed9025d63a0edb9b9edb90f5a5ffd
Sam Glenn <boba@drosera.io>
2026-02-28 15:41:38 -0600

    build: restore libcxx mode and fetch buildtools via git clone

--- cmake/FindLibCXX.cmake
+++ cmake/FindLibCXX.cmake
@@ -1,5 +1,6 @@
 set(LIBCXX_INCLUDE ${DEPS_DIR}/libcxx)
 set(LIBCXXABI_INCLUDE ${DEPS_DIR}/libcxxabi)
+set(BUILDTOOLS_INCLUDE ${DEPS_DIR}/buildtools)
 
 if((LINUX OR MACOS OR ANDROID) AND USE_LIBCXX)
     GitClone(
@@ -7,14 +8,13 @@ if((LINUX OR MACOS OR ANDROID) AND USE_LIBCXX)
         COMMIT ${LIBCXX_COMMIT}
         DIRECTORY ${LIBCXX_INCLUDE}
     )
-    GitFile(
-        URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/${BUILDTOOLS_COMMIT}/third_party/libc++/__config_site
-        DIRECTORY ${LIBCXX_INCLUDE}/include/__config_site
+    GitClone(
+        URL https://chromium.googlesource.com/chromium/src/buildtools.git
+        COMMIT ${BUILDTOOLS_COMMIT}
+        DIRECTORY ${BUILDTOOLS_INCLUDE}
     )
-    GitFile(
-        URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/${BUILDTOOLS_COMMIT}/third_party/libc++/__assertion_handler
-        DIRECTORY ${LIBCXX_INCLUDE}/include/__assertion_handler
-    )
+    file(COPY ${BUILDTOOLS_INCLUDE}/third_party/libc++/__config_site DESTINATION ${LIBCXX_INCLUDE}/include)
+    file(COPY ${BUILDTOOLS_INCLUDE}/third_party/libc++/__assertion_handler DESTINATION ${LIBCXX_INCLUDE}/include)
     GitClone(
         URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxxabi.git
         COMMIT ${LIBCXX_ABI_COMMIT}
